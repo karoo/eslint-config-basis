0f507835517c43497ac788b0c603cf2a101462dd
karoo <nt10@163.com>
2016-12-20 11:02:48 +0800

    feat: add configure

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "plugins": [
+    "react"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
   "rules": {
     "space-before-function-paren": [2, "always"]
   }
