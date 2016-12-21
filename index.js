c6f8a194e8ec9289942685a832923b17c23360f8
karoo <nt10@163.com>
2016-12-21 10:27:03 +0800

    feat: modify rules

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ module.exports = {
     // disallow reassigning const variables
     "no-const-assign": "error",
     // disallow duplicate class members
-    "no-dupe-class-members": "error"
+    "no-dupe-class-members": "error",
+    "space-before-function-paren": ["error", {
+        "anonymous": "never",  // is for anonymous function expressions(e.g. function () {}).
+        "named": "never", // is for named function expressions (e.g. function foo () {}).
+        "asyncArrow": "ignore" // is for async arrow function expressions (e.g. async () => {}). 
+    }],
   }
 }
