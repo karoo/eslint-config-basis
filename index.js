4a840f30d5d119628b77eadc6c2544ec5276f683
karoo <nt10@163.com>
2017-02-21 10:28:02 +0800

    feat: add no-x rules

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ module.exports = {
     "no-await-in-loop": "error",
     // disallow unnecessary parentheses
     "no-extra-parens": "error",
+    // disallow calling some Object.prototype methods directly on objects
+    "no-prototype-builtins": "error",
+    // disallow template literal placeholder syntax in regular strings
+    "no-template-curly-in-string": "error",
+    // disallow negating the left operand of relational operators
+    "no-unsafe-negation": "error",
+    // enforce valid JSDoc comments
+    "valid-jsdoc": "error",
     "space-before-function-paren": ["error", {
         "anonymous": "never",  // is for anonymous function expressions(e.g. function () {}).
         "named": "never", // is for named function expressions (e.g. function foo () {}).
