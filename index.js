da2d8b6e6b458d8a9cabc7cbfe52fa266e03c01b
karoo <nt10@163.com>
2017-03-02 15:50:52 +0800

    feat: add some rules

--- index.js
+++ index.js
@@ -166,6 +166,50 @@ module.exports = {
     "no-undefined": "error",
     // disallow the use of variables before they are defined
     "no-use-before-define": "error",
+    // enforce consistent spacing inside array brackets
+    "array-bracket-spacing": "error",
+    // enforce consistent spacing inside single-line blocks
+    "block-spacing": "error",
+    // enforce consistent brace style for blocks
+    "brace-style": "error",
+    // enforce camelcase naming convention
+    "camelcase": "error",
+    // enforce or disallow capitalization of the first letter of a comment
+    "capitalized-comments": "off",
+    // require or disallow trailing commas
+    "comma-dangle": "error",
+    // enforce consistent spacing before and after commas
+    "comma-spacing": "error",
+    // enforce consistent comma style
+    "comma-style": "error",
+    // enforce consistent spacing inside computed property brackets
+    "computed-property-spacing": "error",
+    // enforce consistent naming when capturing the current execution context
+    "consistent-this": "off",
+    // require or disallow newline at the end of files
+    "eol-last": "error",
+    // require or disallow spacing between function identifiers and their invocations
+    "func-call-spacing": "error",
+    // require function names to match the name of the variable or property to which they are assigned
+    "func-name-matching": "error",
+    // require or disallow named function expressions
+    "func-names": "off",
+    // enforce the consistent use of either function declarations or expressions
+    "func-style": "off",
+    // disallow specified identifiers
+    "id-blacklist": "off",
+    // enforce minimum and maximum identifier lengths
+    "id-length": "off",
+    // require identifiers to match a specified regular expression
+    "id-match": "off",
+    // enforce consistent indentation
+    "indent": ["error", 2],
+    // enforce the consistent use of either double or single quotes in JSX attributes
+    "jsx-quotes": "error",
+    // enforce consistent spacing between keys and values in object literal properties
+    "key-spacing": "error",
+    // enforce consistent spacing before and after keywords
+    "keyword-spacing": "error",
     "space-before-function-paren": ["error", {
       "anonymous": "never",  // is for anonymous function expressions(e.g. function () {}).
       "named": "never", // is for named function expressions (e.g. function foo () {}).
