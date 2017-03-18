aadb14148059bb1eb831e0715e045eebb18c10f0
karoo <nt10@163.com>
2017-03-18 23:30:22 +0800

    fix: fixed some errors

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ module.exports = {
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended",
-    "./lib/best-practices",
-    "./lib/errors",
-    "./lib/es6",
-    "./lib/style",
-    "./lib/variables",    
+    "./lib/best-practices.js",
+    "./lib/errors.js",
+    "./lib/es6.js",
+    "./lib/style.js",
+    "./lib/variables.js",    
   ],
   "plugins": [
     "react"
